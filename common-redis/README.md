8c02c5cba7a4495b73c4908946038fc32c947589
兰顺斌 <itodouble@163.com>
2021-03-08 13:47:50 +0800

    redis

---
+++ common-redis/README.md
@@ -0,0 +1,17 @@
+#### 配置使用
+
+扫描包增加 ```com.ruitu.common.redis```
+
+需要配置文件
+```yml
+redis:
+  host: 127.0.0.1
+  port: 6379
+  password: 123456
+  minIdle: 10
+  maxIdle: 300
+  maxTotal: 800
+  timeoutMS: 1000
+  testOnBorrow: true
+  database: 1
+```
---
+++ common-swagger/src/main/java/top/itodouble/swagger/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package top.itodouble.swagger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		List<Parameter> pars = new ArrayList<>();
+		ParameterBuilder accessTokenParam = new ParameterBuilder();
+		ParameterBuilder refreshTokenParam = new ParameterBuilder();
+		accessTokenParam.name("token").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false);
+		pars.add(accessTokenParam.build());
+		pars.add(refreshTokenParam.build());
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.globalOperationParameters(pars);
+	}
+}
