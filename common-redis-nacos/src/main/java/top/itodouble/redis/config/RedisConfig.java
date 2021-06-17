03642363e0ac371643adef8b2c31706f4ced2e09
兰顺斌 <itodouble@163.com>
2021-06-17 11:18:15 +0800

    redis nacos中读取配置

---
+++ common-redis-nacos/src/main/java/top/itodouble/redis/config/RedisConfig.java
@@ -0,0 +1,74 @@
+package top.itodouble.redis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+@RefreshScope
+@Configuration
+public class RedisConfig {
+    @Value("${redis.host}")
+    private String redisHost;
+    @Value("${redis.port}")
+    private Integer redisPort;
+    @Value("${redis.password}")
+    private String redisPassword;
+
+    @Value("${redis.minIdle:10}")
+    private Integer redisMinIdle;
+    @Value("${redis.maxIdle:300}")
+    private Integer redisMaxIdle;
+    @Value("${redis.maxTotal:800}")
+    private Integer redisMaxTotal;
+    @Value("${redis.timeoutMS:1000}")
+    private Integer redisTimeoutMS;
+    @Value("${redis.testOnBorrow:true}")
+    private Boolean redisTestOnBorrow;
+    @Value("${redis.database:1}")
+    private Integer redisDatabase;
+
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMinIdle(redisMinIdle);
+        jedisPoolConfig.setMaxIdle(redisMaxIdle);
+        jedisPoolConfig.setMaxTotal(redisMaxTotal);
+        jedisPoolConfig.setTestOnBorrow(redisTestOnBorrow);
+        return jedisPoolConfig;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnFactory());
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(stringRedisSerializer);
+        return redisTemplate;
+    }
+
+    /**
+     * 交给start 管理
+     * @return
+     */
+    @Bean
+    public JedisConnectionFactory jedisConnFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setPassword(redisPassword);
+        config.setDatabase(redisDatabase);
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory(config);
+        return connectionFactory;
+    }
+}
